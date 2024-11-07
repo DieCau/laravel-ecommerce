9bc2f6331277ddd79fdfac692b5a82438c9539b4
Diego Caucota <diegocaucota@gmail.com>
2024-11-07 01:57:04 -0300

    fix: added the fields of all tables

--- database/migrations/2024_11_07_044039_create_orders_table.php
+++ database/migrations/2024_11_07_044039_create_orders_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+
+            $table->integer('quantity');
+            $table->decimal('total', 8, 2);
+            $table->dateTime('delivery_at')->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('coupon_id')->nullable()->constrained()->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
